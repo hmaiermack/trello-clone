d8ea97fad34f497d9158d388142b67d550279d79
Mack Maier <hmaiermack@gmail.com>
2021-08-27 12:58:32 -0700

    new item components and functionality, card + column components, useFocus hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { FC } from 'react';
 import { AppContainer } from './styles';
+import { AddNewItem } from './AddNewItem';
+import { Column } from "./Column"
+import { Card } from "./Card"
 
-export const App = () => {
+export const App: FC = ({children}) => {
   return (
     <AppContainer>
-      Columns here
+      <Column text="To Do">
+        <Card text="first card" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="first card" />
+      </Column>
+      <Column text="Finished">
+        <Card text="first card" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
     </AppContainer>
   )
 }
